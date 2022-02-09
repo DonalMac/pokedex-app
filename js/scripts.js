c68b146cf31d528cd4688406220684b4bec25071
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-02-09 10:40:23 +0100

    Creation of PokemonList Array with Objects

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// array of objects
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.9,
+    type: ['grass', 'poison']
+  },
+
+  {
+    name: 'Blastoise',
+    height: 1.6,
+    weight: 85.5,
+    type: ['water']
+  },
+
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire']
+  },
+
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    weight: 9.0,
+    type: ['water']
+  },
+
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    weight: 6.0,
+    type: ['electric']
+  },
+
+  {
+    name: 'Ditto',
+    height: 0.3,
+    weight: 4.0,
+    type: ['normal']
+  }
+];
+
+console.log(pokemonList);
+document.write(pokemonList[0]);
