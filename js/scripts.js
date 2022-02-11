ee1e035e84b3a55ddeaa42e0a52d2a79a4ac6358
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-02-11 16:26:16 +0100

    PokemonList wrapped in IIFE, repository used in forEach to write details to screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,65 @@
-// array of objects
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    weight: 6.9,
-    type: ['grass', 'poison']
-  },
+// Pokemonlist wrapped in IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      weight: 6.9,
+      type: ['grass', 'poison']
+    },
 
-  {
-    name: 'Blastoise',
-    height: 1.6,
-    weight: 85.5,
-    type: ['water']
-  },
+    {
+      name: 'Blastoise',
+      height: 1.6,
+      weight: 85.5,
+      type: ['water']
+    },
 
-  {
-    name: 'Charmander',
-    height: 0.6,
-    weight: 8.5,
-    type: ['fire']
-  },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      weight: 8.5,
+      type: ['fire']
+    },
 
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    weight: 9.0,
-    type: ['water']
-  },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      weight: 9.0,
+      type: ['water']
+    },
 
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    weight: 6.0,
-    type: ['electric']
-  },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      weight: 6.0,
+      type: ['electric']
+    },
 
-  {
-    name: 'Ditto',
-    height: 0.3,
-    weight: 4.0,
-    type: ['normal']
+    {
+      name: 'Ditto',
+      height: 0.3,
+      weight: 4.0,
+      type: ['normal']
+    }
+  ];
+  function add (item) {
+    pokemonList.push(item);
   }
-];
 
-pokemonList.forEach(function (name) {
+  function getAll () {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+// GetAll() called on repository and iterated through forEach loop
+pokemonRepository.getAll().forEach(function (name) {
   // writes the pokemon name and height
   document.write(name.name + ' height:(' + name.height + ')');
 
