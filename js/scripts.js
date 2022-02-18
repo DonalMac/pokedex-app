381c480960f2bec25574c4f0602ff88826ab83aa
DonalMac <98590507+DonalMac@users.noreply.github.com>
2022-02-18 14:02:04 +0100

    js/scripts updated to create listed buttons of the pokemon repository
    validating type of passed parameter to be an object & object keys are correct.
    Updated CSS so list floats in middle of page and can resize formobiles

--- js/scripts.js
+++ js/scripts.js
@@ -43,13 +43,17 @@ let pokemonRepository = (function () {
       type: ['normal']
     }
   ];
+
+  // function to add a pokemon to the pokemonList
   function add (item) {
-    // validating type of passed parameter to be an object
-    if (typeof item === 'object' && Object.keys(item)[0] === 'name') {
-    pokemonList.push(item);
-        document.write('Pokemon added <br>');
-    } else {
-      document.write('Not an object <br>');
+    // validating type of passed parameter to be an object & object keys are correct
+    if (typeof item === 'object' &&
+      'name' in item &&
+      'height' in item &&
+      'weight' in item &&
+      'type' in item
+    ) {
+      pokemonList.push(item);
     }
   }
 
@@ -57,30 +61,42 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  // function that creates a button as list item for a passed pokemon
+  function addListItem (pokemon) {
+    let container = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerHTML = pokemon.name;
+    button.classList.add('JS-button');
+    listItem.appendChild(button);
+    container.appendChild(listItem);
+    // add an event listener to the button that will show details of the pokemon when the button is clicked
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
+  // function that gets pokemon details & prints pokemon name to console
+  function showDetails (pokemon) {
+    console.log(pokemon.name);
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-console.log(pokemonRepository.add({
+// Practicing add function
+pokemonRepository.add({
   name: 'Zoey',
   height: 0.6,
   weight: 8.5,
   type: ['Baby']
-}));
+});
 
-console.log(pokemonRepository.getAll());
 // GetAll() called on repository and iterated through forEach loop
-pokemonRepository.getAll().forEach(function (name) {
-  // writes the pokemon name and height
-  document.write(name.name + ' height:(' + name.height + ')');
-
-  if (name.height > '0.6') {
-    document.write(" Wow that's Big!!! <br>");
-
-    // if the pokemon is smaller writes is a smaller Pokemon
-  } else {
-    document.write(' is a smaller Pokemon. <br>');
-  }
+pokemonRepository.getAll().forEach(function (pokemon) {
+  // Creates a list of buttons for each pokemon in the repository
+  pokemonRepository.addListItem(pokemon);
 });
